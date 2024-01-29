e3910c0df357cbff5bddd37ce885cbcff1cb6f1b
Areesha <133148078+Areesha-Rizwan@users.noreply.github.com>
2024-01-29 06:09:50 -1200

    Delete backened directory

--- backened/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connectDB = () =>{
-    mongoose.set('strictQuery', true);
-    const connection = mongoose.connect()
-}
--- backened/db.js
+++
@@ -1,7 +0,0 @@
-const { connect } = require("mongoose")
-
-const connectDb = async () => {
-  return connect(process.env.DB_URI, { dbName: process.env.DB_NAME })
-}
-
-module.exports = { connectDb }
